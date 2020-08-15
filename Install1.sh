c955851b06a3b6594b4f8b25553d28ad950de83f
lctec <69732259+lctec@users.noreply.github.com>
2020-08-15 16:55:08 -0300

    Create Install1.sh

---
+++ Install1.sh
@@ -0,0 +1,17 @@
+#### Se instalaran programas automaticamente
+pacman -S --needed --noconfirm htop
+pacman -S --needed --noconfirm smplayer
+pacman -S --needed --noconfirm base-devel
+pacman -S --needed --noconfirm qbittorrent
+pacman -S --needed --noconfirm pamac
+pacman -S --needed --noconfirm gparted
+pacman -S --needed --noconfirm chromium
+pacman -S --needed --noconfirm youtube-dl
+pacman -S --needed --noconfirm remmina
+pacman -S --needed --noconfirm openshot
+
+
+#### Se instalaran programas automaticamente  desde AUR
+## gotop
+## chrome
+## google drive 0CamlFuse
