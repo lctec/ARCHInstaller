a6cb0dce66e68afa10ce426f1a34519958c1e360
lctec <69732259+lctec@users.noreply.github.com>
2020-08-18 20:56:14 -0300

    Update 03-xorg.sh

--- 03-xorg.sh
+++ 03-xorg.sh
@@ -1,2 +1,3 @@
-pacman -Sy --needed --noconfirm xorg-server xorg-apps xorg-xinit xorg-xkill xorg-xinput xf86-input-libinput mesa
-  modprobe uinput
+pacman -Sy --needed --noconfirm xorg-server xorg-apps xorg-xinit xorg-xkill xorg-xinput xf86-input-libinput mesa weston xorg-server-xwayland
+modprobe uinput
+pacman -S --asdeps --needed cairo fontconfig freetype2
