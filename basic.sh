4de7b7fc744cc2c16310de5a0b6893951d11482b
lctec <69732259+lctec@users.noreply.github.com>
2020-08-18 20:50:30 -0300

    Create basic.sh

---
+++ basic.sh
@@ -0,0 +1,2 @@
+  pacman -S --needed --noconfirm bc rsync mlocate bash-completion pkgstats arch-wiki-lite zip unzip unrar p7zip lzop cpio avahi nss-mdns alsa-utils alsa-plugins pulseaudio pulseaudio-alsa ntfs-3g dosfstools exfat-utils f2fs-tools fuse fuse-exfat autofs mtpfs
+  timedatectl set-ntp true
