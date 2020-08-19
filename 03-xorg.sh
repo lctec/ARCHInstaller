f25995b075a3a6294de354452a0a166fbde82e5f
lctec <69732259+lctec@users.noreply.github.com>
2020-08-18 21:25:03 -0300

    Update 03-xorg.sh

--- 03-xorg.sh
+++ 03-xorg.sh
@@ -1,3 +1,4 @@
 pacman -Sy --needed --noconfirm xorg-server xorg-apps xorg-xinit xorg-xkill xorg-xinput xf86-input-libinput mesa weston xorg-server-xwayland
 modprobe uinput
 pacman -S --asdeps --needed cairo fontconfig freetype2
+bash 04-Video_Card.sh
